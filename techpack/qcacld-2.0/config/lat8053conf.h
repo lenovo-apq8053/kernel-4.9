b1852a12460b903a8ed0c9f6eff76b637cad9088
lifeng <lifeng@codeaurora.org>
2018-04-24 18:53:51 +0800

    qcacld-2.0: make qcacld-2.0 compiled in techpack
    (cherry picked from commit f585a1dcff26c7bcd169c68a24191a0275458a6f)

---
+++ techpack/qcacld-2.0/config/lat8053conf.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017-2018, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#define CONFIG_QCA_CLD_WLAN 1
+#define CONFIG_WLAN_FEATURE_11W 1
+#define CONFIG_QCOM_LTE_COEX 1
+#define CONFIG_QCA_WIFI_AUTOMOTIVE_CONC 1
+#define CONFIG_LINUX_QCMBR 1
+#define CONFIG_PER_VDEV_TX_DESC_POOL 1
+#define CONFIG_PER_VDEV_TX_DESC_POOL 1
+#define CONFIG_QCA_LL_TX_FLOW_CT 1
+#define CONFIG_QCA_TXDESC_SANITY_CHECKS 1
+#define CONFIG_HIF_USB 1
